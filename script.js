321134a5fe6f36a20d065f7388398c04d716533e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2025-01-13 12:01:15 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+// Set Up Variables
+let botName = "professorCoder";
+let botLocation = "Planet Earth";
+let favoriteLanguage = "JavaScript";
+
+// Apply String Concatenation
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = "In " + favoriteLanguage + ", you will learn about strings, arrays, functions, loops and more.";
+console.log(codingFact);
+
+// Reassign variables
+codingFact = "In " + favoriteLanguage + ", you will also learn about methods to strings.  For example, the include() method.";
+console.log(codingFact);
+codingFact = "In " + favoriteLanguage + ", you will learn how to validate user input.";
+
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
